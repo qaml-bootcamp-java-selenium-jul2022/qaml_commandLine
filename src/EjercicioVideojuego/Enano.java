9c462478bae13a3f8c4c18c19c4bba3ef7cbade2
Jennifer Ramirez <jxr20920@dexcom.com>
2022-07-28 21:32:52 -0500

    Clases del ejercicio del videojuego
    Clases del ejercicio del videojuego
    Enao, personaje, elfo, Humano

---
+++ src/EjercicioVideojuego/Enano.java
@@ -0,0 +1,18 @@
+package EjercicioVideojuego;
+
+public class Enano extends Personaje {
+    @Override
+    public void Arquero() {
+        System.out.println("El Enano se convierte en arquero despues de 5 batallas.");
+    }
+
+    @Override
+    public void Guerrero() {
+        System.out.println("El Enano se convierte en arquero despues de 1 batalla.");
+    }
+
+    @Override
+    public void Mago() {
+        System.out.println("El Enano se convierte en arquero despues de acompañar al humano en 10 batallas.");
+    }
+}
---
+++ src/EjercicioVideojuego/Personaje.java
@@ -0,0 +1,7 @@
+package EjercicioVideojuego;
+
+public abstract class Personaje {
+    public abstract void Arquero();
+    public abstract void Guerrero();
+    public abstract void Mago();
+}
