de9be68987b7b6821276abbe6566cbfb5ea09919
Alvaro Meraz <alvmmn@gmail.com>
2022-08-03 21:22:15 -0500

    Adding Circle exercise
    Adding Circle package and Circulo class

---
+++ src/Circle/Circulo.java
@@ -0,0 +1,34 @@
+package Circle;
+
+public class Circulo {
+
+    private Double radio = 1.00;
+    public Circulo(){
+
+    }
+    public Circulo(Double radio){
+        this.radio = radio;
+
+    }
+
+    public Double getRadio(){
+        return radio;
+    }
+
+    public void setRadio(Double radio){
+        this.radio = radio;
+    }
+
+    public Double getArea(){
+
+        return ((radio * radio) * Math.PI);
+    }
+
+    public Double getCircunferencia(){
+        return (2 * Math.PI * radio);
+    }
+    @Override
+    public String toString(){
+        return "Mi Area es: " + getArea() + " y mi La circunferencia es: " + getCircunferencia();
+    }
+}
---
+++ src/Circle/Main.java
@@ -0,0 +1,13 @@
+package Circle;
+
+public class Main {
+    public static void main(String[] args) {
+        Circulo Circulo1 = new Circulo(2.50);
+
+//        System.out.println("El valor del radio es: "+Circulo1.getRadio());
+//        System.out.println("El Area del Circulo es: "+Circulo1.getArea());
+//        System.out.println("La Circunferencia del Circulo es: "+Circulo1.getCircunferencia());
+          System.out.println(Circulo1.toString());
+
+    }
+}
