84620e505e208f3aebaaec3a137a759d399e23dd
luisgerardomzo024 <luisgerardomzo024@gmail.com>
2022-08-09 12:18:25 -0500

    Cambios en Paquete Zoologico

--- src/Zoologico/Aviario.java
+++
@@ -1,8 +0,0 @@
-package Zoologico;
-
-public abstract class Aviario extends Main {
-    private String casa = "===== El animal vive en el aviario =====";
-    public String getCasa() {
-        return casa;
-    }
-}
--- src/Zoologico/Terrestre.java
+++
@@ -1,9 +0,0 @@
-package Zoologico;
-
-public abstract class Terrestre extends Main{
-    private String casa = "====== El animal vive en un lugar terrestre =====";
-
-    public String getCasa() {
-        return casa;
-    }
-}
