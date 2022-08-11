8d79ee72c0d32380b16b539438244476076b2097
luisgerardomzo024 <luisgerardomzo024@gmail.com>
2022-08-11 15:35:31 -0500

    Update MainEjerciciosColecciones.java
    Eliminar codigo duplicado

--- src/EjerciciosColecciones/MainEjerciciosColecciones.java
+++ src/EjerciciosColecciones/MainEjerciciosColecciones.java
@@ -1,25 +1,12 @@
 package EjerciciosColecciones;
 
 import EjercicioVideojuego.Elfo;
-
-import sun.lwawt.macosx.CSystemTray;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class MainEjerciciosColecciones {
-
-
-    public static void main (String args[]){
-        Map<Integer, Elfo> mapaDeElfos = new TreeMap<>();
-Elfo elfo1 = new Elfo ();
-Elfo elfoJava = new Elfo();
-
-    }
-
-
 
     public static void main(String args[]) {
         Map<Integer, Elfo> mapaDeElfos = new TreeMap<>();
